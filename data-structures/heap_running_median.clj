cec9dd7bf5aa8f079d4801c540dfda27085b8102
Andre Ambrosio Boechat <boechat107@gmail.com>
2016-11-09 12:16:27 -0200

    Reference to improve the solution for 'running median'

--- data-structures/heap_running_median.clj
+++ data-structures/heap_running_median.clj
@@ -1,4 +1,16 @@
+;; Reference:
+;; https://en.wikipedia.org/wiki/Binary_heap
+;;
+;; "It is possible to modify the heap structure to allow extraction of both the
+;; smallest and largest element in {\displaystyle O} O {\displaystyle (\log n)}
+;; (\log n) time.[11] To do this, the rows alternate between min heap and max
+;; heap. The algorithms are roughly the same, but, in each step, one must
+;; consider the alternating rows with alternating comparisons. The performance
+;; is roughly the same as a normal single direction heap. This idea can be
+;; generalised to a min-max-median heap."
+
 (defn simple-median
+  "Simple implementation using 'sort' multiple times."
   [xs]
   (let [drop-num (fn [xs] (let [n (count xs)]
                             (if (< n 3) 0 (-> (count xs)
